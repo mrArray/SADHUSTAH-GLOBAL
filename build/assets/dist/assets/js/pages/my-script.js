1b6eb2e66e32cf6ee6e7fb971b7f8f6db5de4d23
maheer <nouramousser@gmail.com>
2021-03-23 21:50:28 +0000

    transfer

--- build/assets/dist/assets/js/pages/my-script.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-/*!******************************************!*\
-  !*** ../demo7/src/js/pages/my-script.js ***!
-  \******************************************/
-/*! unknown exports (runtime-defined) */
-/*! runtime requirements:  */
-
-/******/ })()
-;
