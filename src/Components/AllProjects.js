8caec6241ddc4997a366946c32629ced97b9a114
maheer <nouramousser@gmail.com>
2021-04-16 09:24:01 +0000

    Token

--- src/Components/AllProjects.js
+++ src/Components/AllProjects.js
@@ -36,14 +36,13 @@ export default class AllProjects extends Component {
         showProjectManager: user.profile.user_groups.includes("PROJECT MANAGER"),
       });
     }
-    const username = 'admin'
-    const password = 'Pass@1234'
-    const token = Buffer.from(`${username}:${password}`, 'utf8').toString('base64')
+    const mytoken = AuthLogin.getCurrentUser();
+    const token = mytoken.token;
     axios.get("https://ecological.chinikiguard.com/projects/api/list/?all_record=1&for_user=true",
       {
         headers:
         {
-          'Authorization': `Basic ${token}`,
+          'Authorization': `Token ${token}`,
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'GET,POST,HEAD,OPTIONS',
           'Access-Control-Allow-Credentials': true
